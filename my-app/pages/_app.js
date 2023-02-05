cd0e8b1cbc7ff6589679b14f57804c905e9bd1c8
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-02-05 12:46:22 +0530

    wallet

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -4,15 +4,46 @@ import {
   getDefaultWallets,
   RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
+import { configureChains, createClient, goerli, WagmiConfig, Chain } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+
+
+export const mantle ={
+  id:5001,
+  name:"Mantle Testnet",
+  network:"mantle",
+  iconUrl:"https://i.imgur.com/Q3oIdip.png",
+  iconBackground:"#000000",
+  nativeCurrency:  {
+    decimals:18,
+    name:'BIT',
+    symbol:'BIT'
+  },
+  rpcUrls:{
+    default:{
+      http:["https://rpc.testnet.mantle.xyz"]
+    },
+  },
+  blockExplorers:{
+    default:{name:"Mantle Testnet Explorer", url:"https://explorer.testnet.mantle.xyz"}
+  },
+  testnet:true
+
+}
+
+
+
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, goerli],
+  [mantle, mainnet, polygon, optimism, arbitrum, goerli],
   [
-    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    publicProvider()
+    jsonRpcProvider({
+      rpc:chain=>({http:chain.rpcUrls.default.http[0]})
+
+    })
   ]
 );
 const { connectors } = getDefaultWallets({
