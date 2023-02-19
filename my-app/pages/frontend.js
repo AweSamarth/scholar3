64c8f7d49a393c6b364739bcf3f5b09d709a27b8
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-19 15:45:27 +0530

    logo added

--- my-app/pages/frontend.js
+++ my-app/pages/frontend.js
@@ -24,9 +24,10 @@ export default function Home() {
       <main className='flex flex-col justify-between items-center p-24 min-h-screen'>
         <div className={"display-inherit ,justify-inherit ,items-inherit, text-xs , w-full ,z-2 ,font-mono"}>
         <div className = "h-[100%] w-[35%] z-10 top-0 pt-[20px] flex justify-center absolute left-0 bg-[#2F2E41]"> 
-            <div className=" self-center relative border-red-500 h-[100%] flex">
+          <div className=" self-centre relative">
+          <p className="text-[20px] ml-2 text-white font-Michroma h-0 mb-[37%]">SCHOLAR3</p>
               <Image
-                className=" self-center"
+                className=""
                 height={1002}
                 width={700}
                 style={{objectFit:"cover", }}
@@ -40,7 +41,7 @@ export default function Home() {
                 
                   <h2 className="mt-[18%] font-Inconsolata font-extrabold text-[40px] leading-[50px] pl-5 pb-10" >Create Researcher Profile!</h2>
                   <input
-                    className="w-[310px] h-[34px] font-Inconsolata bg-white rounded-lg border-none font-medium text-base leading-14 text-center
+                    spellCheck="false" className="w-[310px] h-[34px] font-Inconsolata bg-white rounded-lg border-none font-medium text-base leading-14 text-center
                     "
                     type="text"
                     placeholder="Username"
