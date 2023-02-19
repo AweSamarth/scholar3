b5ac70357cb2721683346339289d9e1d0e426b64
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-19 21:52:11 +0530

    commit 8

---
+++ my-app/pages/mockData.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+
+export default [
+    {
+        id:1,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:2,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:4,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:5,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:6,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:7,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:8,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:9,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    },
+    {
+        id:10,
+        title:"n gram model",
+        researcherName:"Sam",
+        dop:"3 Dec,2022",
+    }
+];
