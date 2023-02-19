baf1e2f1a6ce56eb8996388f2d0841c662ec788b
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-02-19 19:58:13 +0530

    evening commit 4

--- my-app/pages/components/ResearchCard.js
+++ my-app/pages/components/ResearchCard.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Image from "next/image";
 export default function ResearchCard(props) {
+  console.log(props.dop.toString());
   return (
-    <div className="flex flex-row mt-[5%] ml-[24.8%]">
+    <div className="flex flex-row mt-[4%] ml-[24.8%]">
       <div className="flex flex-row flex-wrap ">
         <div className="column">
-          <div className=" h-fit w-[100px]">gi
-            <Image width={200} height={100} src="/undraw avtaar.png" className="h-[100px]"></Image>
+          <div className=" h-fit w-[120px]">
+            <Image width={200} height={100} src="/undraw avtaar.png" className="h-[117px]"></Image>
           </div>
         </div>
         <div className="flex flex-col flex-1 ">
-          <div className="bg-[#808080] h-[100px] w-[700px] pl-[20px] rounded-tr-lg">
-            <h2 className=" pl-[15px] text-[35px] font-Michroma">{props.title}</h2>
-            <p className=" pl-[20px] font-Inconsolata">{`Author:- ${props.researcherName}`}</p>
-            <p className="flex justify-end pr-[3px] font-Inconsolata">{`D.O.P: ${props.dop}`}</p>
+          <div className="bg-[#808080] h-fit w-[700px] pl-[20px] rounded-tr-lg">
+            <h2 className=" pl-[15px] text-[40px] font-Michroma font-[500]">{props.title}</h2>
+            <p className=" pl-[20px] text-[22px] font-[400] font-Inconsolata">{`Author: ${props.researcherName}`}</p>
+            <p className="flex justify-end pr-[3px] font-[700] font-Inconsolata">{`D.O.P: ${props.dop}`}</p>
           </div>
         </div>
       </div>
